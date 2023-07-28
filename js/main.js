2f1c60a095f8448f1279b6a334e8ccb93e7c4969
Suman Karmakar <sumankarmakar.sam@gmail.com>
2023-07-28 11:57:40 +0530

    style & feat :  css elements & complete navbar

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@
 // nav hide
 
 // Swiper Slider
-
+let swiper = new Swiper (".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    }
+})
 // counter design
 
 // our parter
